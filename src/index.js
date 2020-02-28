6b132df4a5e550f4f06ac7e052d13369d1d650a7
Danny Mayasich <dmayasich@macmini-ops05.avtr.net>
2020-02-28 15:35:06 -0800

    wrap <TaskList> with <Droppable> and wrap <Task> with <Draggable> and add the children {...provided.___} to each, also replace 'innerRef' (deprecated) with 'ref' - currently able to drag tasks within a column, but they don't stay (yet)

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import { DragDropContext } from 'react-beautiful-dnd'
 class App extends React.Component {
   state = initialData
 
+  onDragEnd = result => {
+    // ToDo: reorder our column
+  }
+
   render() {
     return (
-      <DragDropContext
-
-      >
+      <DragDropContext onDragEnd={this.onDragEnd}>
         {this.state.columnOrder.map((columnId) => {
           const column = this.state.columns[columnId]
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
--- src/task.jsx
+++ src/task.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Draggable } from 'react-beautiful-dnd'
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   padding: 8px;
   borderr-adius: 2px;
   margin-bottom: 8px;
+  background-color: white;
 `
 
 export default class Task extends React.Component {
 
   render() {
 
-    return <Container>{this.props.task.content}</Container>
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            // innerRef={provided.innerRef}     // deprecated >> replaced with 'ref'
+            ref={provided.innerRef}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    )
   }
 }
