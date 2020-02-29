3f5f7a491d3df9a1499f655567efc19068cf9de7
Danny Mayasich <dmayasich@macmini-ops05.avtr.net>
2020-02-28 18:23:51 -0800

    complete <index.js>'s 'onDragEnd()' function that uses the 'result' argument to rearrange the column's array and setState() with the newly arranged tasks

--- src/index.js
+++ src/index.js
@@ -8,7 +8,36 @@ class App extends React.Component {
   state = initialData
 
   onDragEnd = result => {
-    // ToDo: reorder our column
+    const { destination, source, draggableId } = result
+    if (!destination) return
+
+    // Check to see if location of draggable even moved
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) return
+
+    const column = this.state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+
+    // Move taskId from OLD idx to NEW idx in arr
+    newTaskIds.splice(source.index, 1) // From starting index, remove 1 item
+    newTaskIds.splice(destination.index, 0, draggableId) // From destination index, remove nothing, insert draggableId (taskId)
+  
+    const newColumn = {
+      ...column, // id, title
+      taskIds: newTaskIds // New array with the 'dragged' rearrangement 
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn // Overrides existing column (if there was another column dragged into -- good practice regardless)
+      }
+    }
+
+    this.setState(newState)
   }
 
   render() {
@@ -26,3 +55,19 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// ------------- example of 'result' object passed into 'onDragEnd()' >>> user dragged it from index 0 >> index 1 -------------------------
+
+// const resut = {
+//   draggableId: 'task-1',
+//   type: 'TYPE',
+//   reason: 'DROP', 
+//   source: {
+//     droppableId: 'column-1',
+//     index: 0
+//   },
+//   destination: {
+//     droppableId: 'column-1',
+//     index: 1
+//   }
+// }
